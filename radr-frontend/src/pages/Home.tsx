07637a1eccd593b4ae5c7da4f79ec68a627f7752
squid-ney <sydney.achinger@gmail.com>
2024-02-27 09:18:29 -0500

    Add example DataGrid into project.

--- radr-frontend/src/pages/Home.tsx
+++ radr-frontend/src/pages/Home.tsx
@@ -1,7 +1,9 @@
+import {VariantGrid} from '../components/VariantGrid/VariantGrid.tsx'
 export const Home = () => {
-
-
     return (
+        <>
         <h1>RADR</h1>
+        <VariantGrid/>
+        </>
   )
 }
