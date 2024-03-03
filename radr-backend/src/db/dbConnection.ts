1a40e3ceb13c9599d1d7ede0b82e6958240095ba
squid-ney <sydney.achinger@gmail.com>
2024-03-03 16:42:02 -0500

    Setup eslint & prettier on BE

--- radr-backend/src/db/dbConnection.ts
+++ radr-backend/src/db/dbConnection.ts
@@ -1,9 +1,9 @@
 export const DB_CONNECTION = {
-    host : '127.0.0.1',
-    port : 3307,
-    user : process.env.username,
-    password : process.env.password,
-    database : process.env.database
-  }
-  
-export const DB_CLIENT = 'mysql2'
+  host: "127.0.0.1",
+  port: 3307,
+  user: process.env.username,
+  password: process.env.password,
+  database: process.env.database,
+};
+
+export const DB_CLIENT = "mysql2";
