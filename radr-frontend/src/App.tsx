d1fdc188221663cfb9ab11fbd326f4abfeef83c1
squid-ney <sydney.achinger@gmail.com>
2024-03-12 20:57:02 -0400

    Small styles

--- radr-frontend/src/App.tsx
+++ radr-frontend/src/App.tsx
@@ -1,9 +1,21 @@
 import { Home } from "./pages/Home";
+import { Typography, AppBar } from "@mui/material";
+import { Box, styled } from "@mui/system";
 
+const Content = styled(Box)(({ theme }) => ({
+  paddingTop: theme.spacing(10),
+}));
 function App() {
   return (
     <div className="App">
-      <Home />
+      <AppBar color="primary">
+        <Typography variant="h3" component="h1">
+          RADR
+        </Typography>
+      </AppBar>
+      <Content>
+        <Home />
+      </Content>
     </div>
   );
 }
