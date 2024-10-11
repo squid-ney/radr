621279d8c4e99b10f07136935da831beb381f057
Sydney Achinger <sydneynicoleachinger@spotify.com>
2024-10-11 15:08:30 -0400

    styling clean up

--- radr-backend/src/db/knexfile.js
+++ radr-backend/src/db/knexfile.js
@@ -2,9 +2,9 @@ export default {
   client: "mysql2",
   connection: {
     database: "radr",
-    user: process.env.username,
-    password: process.env.password,
-    port: process.env.database,
+    user: 'radr-user',//process.env.username,
+    password: 'jaiwj28ru823hefh', //process.env.password,
+    port: 3307 //process.env.port,
   },
   migrations: {
     tableName: "knex_migrations",
