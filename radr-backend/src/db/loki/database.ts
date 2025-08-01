851c1c50f24f92680d54b53aeb2f0cc267d1316d
squid-ney <sydney.achinger@gmail.com>
2025-07-31 22:32:20 -0400

    Cleanup dep

--- radr-backend/src/db/loki/database.ts
+++ radr-backend/src/db/loki/database.ts
@@ -1,34 +1,45 @@
-import loki from 'lokijs';
-import pkg from 'xlsx';
+import loki from "lokijs";
+import pkg from "xlsx";
 const { readFile, utils } = pkg;
-import {RadrDatabase} from "../RadrDatabase";
+import { RadrDatabase } from "../RadrDatabase";
+import { fileURLToPath } from "url";
+import path from "path";
 
-export class LokiDatabase implements  RadrDatabase{
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export class LokiDatabase implements RadrDatabase {
   db: loki;
 
   constructor() {
-      this.db = new loki('radr.db');
-      const variants = this.db.addCollection('variants');
-      const workbook = readFile('/Users/sydneynicoleachinger/Repositories/personal/radr/radr-backend/src/db/loki/data/radr_data_10-11.csv');
-      const sheetName = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[sheetName];
+    this.db = new loki("radr.db");
+    const variants = this.db.addCollection("variants");
 
-      const jsonData = utils.sheet_to_json(worksheet);
-      variants.insert(jsonData);
+    const filePath = path.join(__dirname, "data", "radr_data_10-11.csv");
+
+    const workbook = readFile(filePath);
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+
+    const jsonData = utils.sheet_to_json(worksheet);
+    variants.insert(jsonData);
   }
-  #mapVariants = (variants:any[]): any[]=> {
+  #mapVariants = (variants: any[]): any[] => {
     if (!variants) return [];
     variants?.forEach((variant) => delete variant.meta);
-    return variants?.map((variant) => ( {...variant, variant_id: variant.$loki}))
-  }
+    return variants?.map((variant) => ({
+      ...variant,
+      variant_id: variant.$loki,
+    }));
+  };
 
   getVariants(): Promise<any[]> {
-    const variants = this.db.getCollection('variants').find();
+    const variants = this.db.getCollection("variants").find();
     return Promise.resolve(this.#mapVariants(variants));
   }
 
-  getVariantById(id: number): Promise<any>  {
-    const variant = this.db.getCollection('variants').get(id)
+  getVariantById(id: number): Promise<any> {
+    const variant = this.db.getCollection("variants").get(id);
     return Promise.resolve(this.#mapVariants([variant])[0]);
   }
 }
